408b16c0dc91e504800cea1516e369b0e2011373
Stefan Stankovic <s@stefans.dev>
2020-04-18 08:25:05 +0200

    new video background

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,22 +10,24 @@ import BgVideo from '../images/bg_optimized.mp4'
 const Homepage = () => (
     <>
         <SEO title="Stefan Stankovic , stefans.dev | Web Design and Development" />
-        <Nav />
-        <SocialSidebar />
-        <div className="hero">
-            <video autoPlay loop>
-                <source src={BgVideo} type="video/mp4" />
-                Your browser does not support the video tag.
-            </video>
+        <div className="page_wrap">
+            <Nav />
+            <SocialSidebar />
+            <div className="hero">
+                <video autoPlay loop>
+                    <source src={BgVideo} type="video/mp4" />
+                    Your browser does not support the video tag.
+                </video>
 
-            <div className="content">
-                <h1>
-                    Hi, I'm Stefan, <br />I do Web Development
-                </h1>
-                <h4>Web Design - Web Development</h4>
-                <div className="buttons">
-                    <Link to="/work">Check my work</Link>
-                    <Link to="/contact">Contact me</Link>
+                <div className="content">
+                    <h1>
+                        Hi, I'm Stefan, <br />I do Web Development
+                    </h1>
+                    <h4>Web Design - Web Development</h4>
+                    <div className="buttons">
+                        <Link to="/work">Check my work</Link>
+                        <Link to="/contact">Contact me</Link>
+                    </div>
                 </div>
             </div>
         </div>
