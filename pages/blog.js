6841f09124bd5eebf678176d59e36e41c2b0fc23
stefansdev <s@stefans.dev>
2021-07-20 00:17:33 +0200

    testing pwa

--- pages/blog.js
+++ pages/blog.js
@@ -6,36 +6,33 @@ import Seo from '../components/Seo';
 import PageTitle from '../components/PageTitle';
 import Article from '../components/elements/Article';
 
-const Blog = ({ posts }) => {
-	console.log(posts);
-	return (
-		<div>
-			<Seo
-				title="Blog | Stefan Stankovic - Full Stack Javascript Developer"
-				description="Full Stack Javascript Developer"
-			/>
-			<Header />
-			<div className="container mx-auto max-w-7xl py-28 relative">
-				<div className="relative">
-					<PageTitle title="BLOG" subtitle="Posts i wrote" />
-				</div>
-				<div className="grid grid-cols-3 gap-4 pt-16">
-					{posts.map((article) => (
-						<Article
-							slug={article.slug}
-							title={article.title}
-							key={article.slug}
-							image={article.image}
-							excerpt={article.excerpt}
-							categories={article.categories}
-						/>
-					))}
-				</div>
+const Blog = ({ posts }) => (
+	<>
+		<Seo
+			title="Blog | Stefan Stankovic - Full Stack Javascript Developer"
+			description="Full Stack Javascript Developer"
+		/>
+		<Header />
+		<div className="container mx-auto max-w-7xl py-28 relative">
+			<div className="relative">
+				<PageTitle title="BLOG" subtitle="Posts i wrote" />
 			</div>
-			<Footer />
+			<div className="grid grid-cols-3 gap-4 pt-16">
+				{posts.map((article) => (
+					<Article
+						slug={article.slug}
+						title={article.title}
+						key={article.slug}
+						image={article.image}
+						excerpt={article.excerpt}
+						categories={article.categories}
+					/>
+				))}
+			</div>
 		</div>
-	);
-};
+		<Footer />
+	</>
+);
 
 export default Blog;
 
