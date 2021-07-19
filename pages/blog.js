f9b76fd82fb396cdd61e0d7cbb2b99c9d48bd132
stefansdev <s@stefans.dev>
2021-07-19 21:35:47 +0200

    blog

--- pages/blog.js
+++ pages/blog.js
@@ -1,12 +1,49 @@
 import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import { getAllPosts } from '../utils/blog';
+import Seo from '../components/Seo';
+import PageTitle from '../components/PageTitle';
+import Article from '../components/elements/Article';
 
-const Blog = () => (
-	<div>
-		<Header />
-		<Footer />
-	</div>
-);
+const Blog = ({ posts }) => {
+	console.log(posts);
+	return (
+		<div>
+			<Seo
+				title="Blog | Stefan Stankovic - Full Stack Javascript Developer"
+				description="Full Stack Javascript Developer"
+			/>
+			<Header />
+			<div className="container mx-auto max-w-7xl py-28 relative">
+				<div className="relative">
+					<PageTitle title="BLOG" subtitle="Posts i wrote" />
+				</div>
+				<div className="grid grid-cols-3 gap-4 pt-16">
+					{posts.map((article) => (
+						<Article
+							slug={article.slug}
+							title={article.title}
+							key={article.slug}
+							image={article.image}
+							excerpt={article.excerpt}
+							categories={article.categories}
+						/>
+					))}
+				</div>
+			</div>
+			<Footer />
+		</div>
+	);
+};
 
 export default Blog;
+
+export async function getStaticProps() {
+	const posts = await getAllPosts();
+	return {
+		props: {
+			posts,
+		},
+	};
+}
--- utils/blog.js
+++ utils/blog.js
@@ -2,20 +2,57 @@ import fs from 'fs';
 import { join } from 'path';
 import matter from 'gray-matter';
 
-const blogDirectory = join(process.cwd(), 'content/posts');
+const blogDirectory = join(process.cwd(), 'content/');
 
 export function getPostBySlug(slug) {
 	const realSlug = slug.replace(/\.md$/, '');
-	const fullPath = join(blogDirectory, `${realSlug}.md`);
+	const fullPath = join(blogDirectory, `posts/${realSlug}.md`);
 	const fileContents = fs.readFileSync(fullPath, 'utf8');
 	const { data, content } = matter(fileContents);
-
 	return { slug: realSlug, meta: data, content };
 }
 
-export function getAllPosts() {
-	const slugs = fs.readdirSync(blogDirectory);
-	const posts = slugs.map((slug) => getPostBySlug(slug));
+// get page by slug
+export function getPageBySlug(slug) {
+	const realSlug = slug.replace(/\.md$/, '');
+	const fullPath = join(blogDirectory, `pages/${realSlug}.md`);
+	const fileContents = fs.readFileSync(fullPath, 'utf8');
+	const { data, content } = matter(fileContents);
+	return { slug: realSlug, meta: data, content };
+}
 
-	return posts;
+// get all posts
+export function getAllPosts() {
+	const files = fs.readdirSync(`${process.cwd()}/content/posts`);
+
+	const posts = files.map((filename) => {
+		const markdownWithMetadata = fs.readFileSync(`content/posts/${filename}`).toString();
+
+		const { data, content } = matter(markdownWithMetadata);
+		const formattedDate = new Date(data.date).toISOString();
+
+		const postList = {
+			...data,
+			date: formattedDate,
+			content,
+		};
+
+		return {
+			slug: filename.replace('.md', ''),
+			...postList,
+		};
+	});
+
+	const postDataSortByDate = posts.sort((a, b) => -a.date.localeCompare(b.date));
+
+	return postDataSortByDate;
+}
+
+// get recent posts (for homepage)
+export function getRecentBlogs() {
+	const allPosts = getAllPosts();
+
+	const recentPosts = allPosts.splice(0, 3);
+
+	return recentPosts;
 }
