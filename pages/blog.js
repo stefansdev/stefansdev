cb362194f687ab5204a6ab890c4f48c9812b2b22
stefansdev <s@stefans.dev>
2021-07-19 06:46:37 +0200

    blog page created

---
+++ pages/blog.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
+const Blog = () => (
+	<div>
+		<Header />
+		<Footer />
+	</div>
+);
+
+export default Blog;
