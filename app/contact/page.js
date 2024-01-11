3549c7878f9e882f0cb32947f18aaeccf688dca0
stefansdev <s@stefans.dev>
2024-01-11 12:39:39 +0100

    meta image

--- app/contact/page.js
+++ app/contact/page.js
@@ -12,6 +12,14 @@ export async function generateMetadata() {
 		openGraph: {
 			title: data.meta_title,
 			description: data.meta_description,
+			images: [
+				{
+					url: `https://stefans.dev/assets/${data.meta_image.id}`,
+					width: data.meta_image.width,
+					height: data.meta_image.height,
+					alt: data.meta_image.title,
+				},
+			],
 		},
 	};
 }
